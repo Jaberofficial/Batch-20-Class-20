76db519223cd0b3c7539eb8781bc3606e6addb6d
Jaberofficial <jaberhossainmojumderononto02@gmail.com>
2026-04-21 21:59:42 +0600

    Javascript Data Structure Array Intro

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+let mark1 = 40;
+let mark2 = 80;
+let mark3 = 33;
+
+let sum = mark1 + mark2;
+
+console.log(mark3);
+console.log(sum);
+
+//1D Array (Index Array)
+let marks = [40, 80, 33];
+console.log(marks[2]);
+
+let arraySum = marks[0] + marks[2];
+console.log(arraySum);
+
+
+
+//Associative Array (Object)
+let person = { name: 'Rahim', age: 32, blood: 'A+', phone: '+88018XXXXXXXX' };
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.blood);
+console.log(person.phone);
+
+// 2D Array:
+let matrix = [
+    [4, 9, 10, 90, 0],
+    [10, 20, 11, 2, 6],
+    [3, 4, 7, 0, 1],
+    [2, 5, 13, 9, 14]
+];
+
+console.log(matrix[1][2]);
+console.log(matrix[2][2]);
+
+
+
